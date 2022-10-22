f5cc7c29b6ddc34e34fe1befdf0baf0485961a48
yogeshkumaryadu <yogeshkumaryadu@gmail.com>
2022-10-22 11:25:41 +0530

    update

--- src/main/java/com/example/gitLearningDemo2/GitLearningDemo2Application.java
+++ src/main/java/com/example/gitLearningDemo2/GitLearningDemo2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitLearningDemo2Application {
 
 	public static void main(String[] args) {
+		System.out.println("google");
 		SpringApplication.run(GitLearningDemo2Application.class, args);
 	}
 
